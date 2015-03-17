097c38cffe333ae10ff119c7c37b22f298607d97
matzika <katerina.iliakopoulou@gmail.com>
2015-03-16 23:55:24 -0400

    updates in the service package

--- src/service/SearchService.java
+++ src/service/SearchService.java
@@ -1,5 +1,19 @@
 package service;
 
+// ALL COMMENTS EVENTUALLY NEED TO BE CONVERTED TO JAVADOC COMMENTS
+
+//ADD ANY OTHER METHODS YOU MIGHT THINK ITS NECESSARY
+//IF A METHOD APPLIES TO ALL API METHODS THEN ADD IT AS AN ABSTRACT METHOD IN THE SERVICE CLASS
+
 public class SearchService {
 
+	//constructor - pass any arguments that might be necessary for the API call
+	public SearchService(){
+		
+	}
+	
+	//implementation of the abstract method requestInfo with respect to the Search API 
+	public void requestInfo(){
+		
+	}
 }
--- src/service/Service.java
+++ src/service/Service.java
@@ -1,5 +1,7 @@
 package service;
 
 public abstract class Service {
-
+	
+	public abstract void requestInfo();
+	
 }
