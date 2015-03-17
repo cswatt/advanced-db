0a418a87ac903c673bb0d8badb4676c30d93134b
cswatt <ceciliawatt@gmail.com>
2015-03-17 14:25:37 -0400

    Updates SearchService and SearchResult

--- src/result/SearchResult.java
+++ src/result/SearchResult.java
@@ -1,5 +1,30 @@
 package result;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import com.jayway.jsonpath.JsonPath;
 
 public class SearchResult extends Result{
+	
+	private String mid;
+	private String id;
+	private String name;
+	private String notable_id;
+	
+	private JSONObject search;
+	
+	public SearchResult(JSONObject search){
+		this.search = search;
+		parse();
+	}
+	
+	private void parse(){
+		mid = JsonPath.read(search,"$.mid").toString();
+		id = JsonPath.read(search,"$.id").toString();
+		name = JsonPath.read(search,"$.name").toString();
+		notable_id = JsonPath.read(search, "$.notable.id").toString();
+	}
 	
 }
--- src/service/SearchService.java
+++ src/service/SearchService.java
@@ -8,9 +8,12 @@ import com.google.api.client.http.HttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.jayway.jsonpath.JsonPath;
 
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.HashSet;
 import java.util.Properties;
+
+import result.Result;
+import result.SearchResult;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -29,46 +32,77 @@ public class SearchService extends Service{
 	private GenericUrl url;
 	JSONParser parser = new JSONParser();
 	
-	//constructor - pass any arguments that might be necessary for the API call
 	public SearchService(String apiKey, String query){
 		this.query = query;
 		this.apiKey = apiKey;
 	}
 	
-	//implementation of the abstract method requestInfo with respect to the Search API 
-	public void requestInfo(int limit){
+	public void requestInfo(){
+		HashSet<String> types = new HashSet<String>();
+		types.add("/people/person");
+		types.add("/book/author");
+		types.add("/film/actor");
+		types.add("/tv/tv_actor");
+		types.add("/organization/organization_founder");
+		types.add("/business/board_member");
+		types.add("/sports/sports_league");
+		types.add("/sports/sports_team");
+		types.add("/sports/professional_sports_team");
+		String mid = "";
+		String notable_id = "";
+		
+		JSONObject found_result = null;
+		
 		try {
-			this.httpTransport = new NetHttpTransport();
-			this.requestFactory = httpTransport.createRequestFactory();
-			this.url = new GenericUrl("https://www.googleapis.com/freebase/v1/search");
-			url.put("query", query);
-			url.put("filter", "(any type:/people/person type:/book/author type:/film/actor type:/tv/tv_actor type:/organization/organization_founder type:/business/board_member type:/sports/sports_league type:/sports/sports_team type:/sports/professional_sports_team)");
-			url.put("limit", limit);
-			url.put("indent", "true");
-			url.put("key", apiKey);
-			this.request = requestFactory.buildGetRequest(url);
-			this.httpResponse = request.execute();
-			
-			JSONObject response = (JSONObject)parser.parse(httpResponse.parseAsString());
-			JSONArray results = (JSONArray)response.get("result");
-		}
-		catch(Exception ex) {
+			boolean found = false;
+			int limit = 5;
+			while(!found && limit <= 20){
+				JSONArray results = search(this.apiKey, this.query,  limit);
+				for (Object result : results){
+					mid = JsonPath.read(result,"$.mid").toString();
+					try{notable_id = JsonPath.read(result, "$.notable.id").toString();}
+					catch (Exception e){notable_id = "";}
+					if (types.contains(notable_id)){
+						found_result = (JSONObject)result;
+						found = true;
+						System.out.println("found");
+						break;
+					}
+				}
+				if (!found) System.out.println(limit + " Search API result entries were considered. None of them of a supported type.");
+				
+				if (limit == 20) System.out.println("nothing found");
+				
+				limit = limit + 5;
+			}
+		} catch (Exception ex) {
 			ex.printStackTrace();
-		}
+		} 
+		Result res = new SearchResult(found_result);
 	}
+	
+
+	public JSONArray search(String apiKey, String query, int limit) throws IOException, ParseException{
+		JSONArray results = null;
+		this.httpTransport = new NetHttpTransport();
+		this.requestFactory = httpTransport.createRequestFactory();
+		this.url = new GenericUrl("https://www.googleapis.com/freebase/v1/search");
+		url.put("query", query);
+		url.put("limit", limit);
+		url.put("indent", "true");
+		url.put("key", apiKey);
+		this.request = requestFactory.buildGetRequest(url);
+		this.httpResponse = request.execute();
+		
+		JSONObject response = (JSONObject)parser.parse(httpResponse.parseAsString());
+		results = (JSONArray)response.get("result");
+		return results;
+	}
+	
 	//for testin' stuff
 	public static void main(String args[]){
 		String key = "AIzaSyDaVrp5DyCfmDx60NFbBBSzPCfK8X4qyho";
 		Service service = new SearchService(key, "bill gates");
 		service.requestInfo();
-		/*JSONArray results = freebaseSearch("AIzaSyCKF0R67loGSSMHjazKxsZNgbvyRFOirmM", "bill gates",  5);
-		if (results.isEmpty()){
-			System.out.println("5 Search API result entries were considered. None of them of a supported type.");
-			return;
-		}
-		String name = JsonPath.read(results.get(0),"$.name").toString();
-		String mid = JsonPath.read(results.get(0),"$.mid").toString();
-		System.out.println("found entity: " + name + ", " + mid);
-		*/
 	}
 }
