c4006ff9dbc47ef38388c8ccf644eb352a4cea7e
cswatt <ceciliawatt@gmail.com>
2015-03-20 13:37:34 -0400

    setter/getter methods for mqlresult

--- src/result/MQLResult.java
+++ src/result/MQLResult.java
@@ -16,7 +16,7 @@ import com.jayway.jsonpath.JsonPath;
 
 public class MQLResult extends Result{
 	private static final long serialVersionUID = -7934442049449016087L;
-	private Map<List<String>, List<String>> map = new HashMap<List<String>, List<String>>();
+	private Map<List<String>, List<String>> mql_map = new HashMap<List<String>, List<String>>();
 	private JSONArray book_results;
 	private JSONArray organization_results;
 	
@@ -31,20 +31,39 @@ public class MQLResult extends Result{
 			String author = JsonPath.read(res,"$.name").toString();
 			List<String> author_tuple = Arrays.asList(author, "Author");
 			List<String> books = JsonPath.read(res,"$./book/author/works_written[*].a:name");
-			map.put(author_tuple, books);
+			mql_map.put(author_tuple, books);
 		}
 		
 		for (Object res : organization_results){
 			String businessperson = JsonPath.read(res,"$.name").toString();
 			List<String> businessperson_tuple = Arrays.asList(businessperson, "Businessperson");
 			List<String> organizations = JsonPath.read(res,"$./organization/organization_founder/organizations_founded[*].a:name");
-			map.put(businessperson_tuple, organizations);
+			mql_map.put(businessperson_tuple, organizations);
 		}
 		
-		Set<List<String>> keys = map.keySet();
+		Set<List<String>> keys = mql_map.keySet();
 		for (List<String> key : keys) {
 			System.out.println(key);
-			System.out.println(map.get(key));
+			System.out.println(mql_map.get(key));
 		}
+	}
+	
+	public void setBookResults(JSONArray book_results){
+		this.book_results = book_results;
+	}
+	public JSONArray getBookResults(){
+		return this.book_results;
+	}
+	public void setOrganizationResults(JSONArray organization_results){
+		this.organization_results= organization_results;
+	}
+	public JSONArray getOrganizationResults(){
+		return this.organization_results;
+	}
+	public void setMQLMap(Map m){
+		this.mql_map = m;
+	}
+	public Map getMQLMap(){
+		return this.mql_map;
 	}
 }
