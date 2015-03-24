608d791f169b589890ef9ab8c6eb48011bf38dbb
cswatt <ceciliawatt@gmail.com>
2015-03-24 12:23:20 -0400

    comments for SearchResult

--- src/result/SearchResult.java
+++ src/result/SearchResult.java
@@ -5,7 +5,11 @@ import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 import com.jayway.jsonpath.JsonPath;
-
+/**
+ * Represents a result from the SearchService
+ * Makes mid, id, name, and notable_id accessible,
+ * though largely only mid is used.
+ */
 public class SearchResult extends Result{
 	
 	private String mid;
@@ -15,17 +19,28 @@ public class SearchResult extends Result{
 	
 	private JSONObject search;
 	
+	/**
+	 * Creates a SearchResult object, calls parse
+	 * @param search
+	 */
 	public SearchResult(JSONObject search){
 		this.search = search;
 		parse();
 	}
 	
+	/**
+	 * Extracts values from JSON
+	 */
 	private void parse(){
 		mid = JsonPath.read(search,"$.mid").toString();
 		id = JsonPath.read(search,"$.id").toString();
 		name = JsonPath.read(search,"$.name").toString();
 		notable_id = JsonPath.read(search, "$.notable.id").toString();
 	}
+	
+	/**
+	 * Methods to get/set the mid, id, name, notable_id
+	 */
 	public void setMid(String s){
 		this.mid = s;
 	}
